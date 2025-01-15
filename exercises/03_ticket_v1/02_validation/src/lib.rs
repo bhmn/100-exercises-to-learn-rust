43e7fe9549fc4582cfeddbc821b16d1ec6a09763
bhmn <dard.cool@gmail.com>
2025-01-15 15:21:42 +0330

    Pc - working on ticket validation...

--- exercises/03_ticket_v1/02_validation/src/lib.rs
+++ exercises/03_ticket_v1/02_validation/src/lib.rs
@@ -1,3 +1,5 @@
+use std::str::Bytes;
+
 struct Ticket {
     title: String,
     description: String,
@@ -18,7 +20,36 @@ impl Ticket {
     // as well as some `String` methods. Use the documentation of Rust's standard library
     // to find the most appropriate options -> https://doc.rust-lang.org/std/string/struct.String.html
     fn new(title: String, description: String, status: String) -> Self {
-        todo!();
+        //todo!();
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+
+        // println!("{:?}", title.bytes().gt(*ff));
+        //panic!("{:?}", title.as_bytes().len());
+
+        if title.as_bytes().len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+
+        if description.as_bytes().len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        };
+        // panic!(
+        //     "{:?} - {:?} - {:?} - {:?}",
+        //     !status.eq("To-Do"),
+        //     !status.eq("Done"),
+        //     !status.eq("In Progress"),
+        //     !status.eq("To-Do") || !status.eq("Done") || !status.eq("In Progress")
+        // );
+
+        if !status.eq("To-Do") && !status.eq("Done") && !status.eq("In Progress") {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+
         Self {
             title,
             description,
