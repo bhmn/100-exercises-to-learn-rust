358e69e249f07526e308fdc8d476f44551d55722
Bhmn <dard.cool@gmail.com>
2025-02-03 20:26:20 +0330

    Pc - 05_ticket_management - 05_iter ...

--- exercises/06_ticket_management/05_iter/src/lib.rs
+++ exercises/06_ticket_management/05_iter/src/lib.rs
@@ -1,3 +1,4 @@
+
 use ticket_fields::{TicketDescription, TicketTitle};
 
 // TODO: Provide an `iter` method that returns an iterator over `&Ticket` items.
@@ -34,8 +35,11 @@ impl TicketStore {
     pub fn add_ticket(&mut self, ticket: Ticket) {
         self.tickets.push(ticket);
     }
-}
 
+    pub fn iter(&self) -> std::slice::Iter<Ticket> {
+        self.tickets.iter()
+    }
+}
 
 #[cfg(test)]
 mod tests {
