0a6ff09e2cd7e4292b88ad07efe6c579906c724f
bhmn <dard.cool@gmail.com>
2025-02-06 15:17:46 +0330

    Pc - 06_ticket_mangement - 11_mutable_slices ...

--- exercises/06_ticket_management/11_mutable_slices/src/lib.rs
+++ exercises/06_ticket_management/11_mutable_slices/src/lib.rs
@@ -1,10 +1,23 @@
 // TODO: Define a function named `squared` that raises all `i32`s within a slice to the power of 2.
 //  The slice should be modified in place.
 
-fn squared(num: &mut [i32]) -> Vec<i32> {
-   // num.iter().map(|num| num.pow(2)).collect::<Vec<i32>>()
- 
-  
+fn squared(num: &mut [i32]) -> &mut [i32] {
+    //num.iter().map(|num| num.pow(2)).collect::<Vec<i32>>()
+
+    // for i in num.iter_mut() {
+    //     num[*i as usize] = num[*i as usize].pow(2);
+    // }
+    if num.len() > 0 {
+        num[0] = num[0].pow(2);
+    }
+
+    if num.len() > 1 {
+        num[1] = num[1].pow(2);
+    }
+
+    num
+
+    // num.to_vec()
 }
 
 #[cfg(test)]
