cfef97a446afd8b7cee6041c7dc551158c60e080
bhmn <dard.cool@gmail.com>
2025-02-18 15:04:44 +0330

    Pc - 07_threads - 12_rw_lock ...

--- exercises/07_threads/11_locks/src/store.rs
+++ exercises/07_threads/11_locks/src/store.rs
@@ -20,7 +20,7 @@ impl TicketStore {
     }
 
     pub fn add_ticket(&mut self, ticket: TicketDraft) -> TicketId {
-        let id = TicketId(self.counter);
+        let id: TicketId = TicketId(self.counter);
         self.counter += 1;
         let ticket = Ticket {
             id,
@@ -28,13 +28,15 @@ impl TicketStore {
             description: ticket.description,
             status: Status::ToDo,
         };
-        todo!();
+        //todo!();
+        self.tickets.insert(id, Arc::new(Mutex::new(ticket)));
         id
     }
 
     // The `get` method should return a handle to the ticket
     // which allows the caller to either read or modify the ticket.
-    pub fn get(&self, id: TicketId) -> Option<todo!()> {
-        todo!()
+    pub fn get(&self, id: TicketId) -> Option<Arc<Mutex<Ticket>>> {
+        //todo!(
+        self.tickets.get(&id).cloned()
     }
 }
