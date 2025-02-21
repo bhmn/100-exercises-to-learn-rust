436e0ca97b9f55d732674e726d423978e2ddd7ce
bhmn <dard.cool@gmail.com>
2025-02-21 09:22:41 +0330

    Pc - 08_futures - 01_async_fn ...

--- exercises/08_futures/01_async_fn/src/lib.rs
+++ exercises/08_futures/01_async_fn/src/lib.rs
@@ -1,4 +1,3 @@
-use anyhow::Ok;
 use tokio::{io::copy, net::TcpListener};
 
 // TODO: write an echo server that accepts incoming TCP connections and
@@ -12,10 +11,11 @@ use tokio::{io::copy, net::TcpListener};
 // - `tokio::net::TcpStream::split` to obtain a reader and a writer from the socket
 // - `tokio::io::copy` to copy data from the reader to the writer
 pub async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
-    let mut d = listener.accept().await.unwrap();
-    let (mut reader, mut writer) = d.0.split();
-    copy(&mut reader, &mut writer).await.err();
-    Ok(())
+    loop {
+        let (mut tcpstream, _) = listener.accept().await?;
+        let (mut reader, mut writer) = tcpstream.split();
+        copy(&mut reader, &mut writer).await?;
+    }
 }
 
 #[cfg(test)]
